fad2c9f4bb1c39e2bec7c9e0d5e325a9394bd037
Rob King <samiamspop@gmail.com>
2017-01-16 16:34:10 -0800

    Latest js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 ﻿/*****************************/
 /*        js Scripts         */
 /*****************************/
-/* - jquery-3.1.1.min.js     */
-/* - modernizr-3.3.1         */
+/* - jquery-3.1.1.min.js moved to CDN */
+/* - modernizr-3.3.1 moved to CDN */
 /* - Materialize v0.97.8    */
 /* - sitewide                */
 /*****************************/

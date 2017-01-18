6f627b07141a83d7b7115351fc065785d6af123b
rob.king <rob.king@saturnodesign.com>
2017-01-18 15:59:55 -0800

    Added materialize

--- js/sitewide.js
+++ js/sitewide.js
@@ -1,6 +1,14 @@
 /**********/
 /* Custom */
 /**********/
-$(document).ready(function () {
-    $('.carousel.carousel-slider').carousel({ full_width: true });
-});
+(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+        //$('.carousel.carousel-slider').carousel({ full_width: true });
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+//$(document).ready(function () {
+//});
